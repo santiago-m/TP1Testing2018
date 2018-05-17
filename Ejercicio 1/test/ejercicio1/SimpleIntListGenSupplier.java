3b0be9ec4365d2cb97a0f0c336c6e1e1c523faf2
Santiago Maria <santimaria93@gmail.com>
2018-05-17 12:32:25 -0300

    Increased quantity of generated lists

--- Ejercicio 1/test/ejercicio1/SimpleIntListGenSupplier.java
+++ Ejercicio 1/test/ejercicio1/SimpleIntListGenSupplier.java
@@ -22,7 +22,7 @@ public class SimpleIntListGenSupplier extends ParameterSupplier {
 		Random value = new Random();
 		Random length = new Random();
 		
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 50; i++) {
 			int listSize = length.nextInt(maxLength) + minLength;
 			List<Integer> generatedList = new ArrayList<Integer>(listSize); 
 			
